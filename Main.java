f63ac3518c071b23f2972b8dd948da1ecfeb5998
alelomelir <alejandralomeli2712@gmail.com>
2024-08-26 18:35:26 -0600

    array 1000 elements

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear un arreglo de 1000 elementos
+        int[] randomArray = new int[1000];
+        
+        // Instanciar un objeto de la clase Random
+        Random rand = new Random();
+        
+        // Llenar el arreglo con números aleatorios
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = rand.nextInt(); // Genera un número aleatorio
+        }
+        
+        // Imprimir el arreglo (opcional)
+        for (int num : randomArray) {
+            System.out.println(num);
+        }
+    }
+}
